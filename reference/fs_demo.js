6cebca3c6cd2337cda1e814d9065804b8847cd05
salamanderctesiphon <swbrookshire@gmail.com>
2023-08-08 17:44:06 -0500

    oprimized sub-optimal code

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -8,7 +8,21 @@ const path = require('path');
 }); */
 
 //Create and write to file
-fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), 'Hello World!', err => {
+/* fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), 'Hello World!', err => {
   if(err) throw err;
   console.log("File written to...");
+
+  fs.appendFile(path.join(__dirname, '/test', 'hello.txt'), 'I love node js', err => {
+    if(err) throw err;
+    console.log("File written to...");
+  })
 })
+ */
+
+//Read file
+fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf-8', (err, data) => {
+  if(err) throw err;
+  console.log(data);
+})
+
+
