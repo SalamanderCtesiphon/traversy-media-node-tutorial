75bf826891a5d8484679707bd90a365c84a2df68
salamanderctesiphon <swbrookshire@gmail.com>
2023-08-09 13:11:24 -0500

    explored the os module of node js

---
+++ reference/os_demo.js
@@ -0,0 +1,22 @@
+const os = require('os');
+
+//platform
+console.log(os.platform());
+
+// CPU arch
+console.log(os.arch());
+
+// CPU Core info
+console.log(os.cpus());
+
+// Free Memory
+console.log(os.freemem());
+
+//Total Memory 
+console.log(os.totalmem());
+
+//Home dir
+console.log(os.homedir());
+
+// Uptime
+console.log(os.uptime());
